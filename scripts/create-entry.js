97979db5e2a0db4308faad91395bf81d4450d7b6
heineiuo <heineiuo@gmail.com>
2020-06-06 16:47:59 +0800

    fix(ci): did not fail after run build

--- scripts/create-entry.js
+++ scripts/create-entry.js
@@ -5,13 +5,17 @@ const yargs = require("yargs");
 const argv = yargs.argv;
 
 async function createEntry() {
-  await fs.mkdir(path.resolve(process.cwd(), "./src"));
-  const content = `export * from '${argv.pkg}';`;
-  await fs.writeFile(
-    path.resolve(process.cwd(), "./src/index.ts"),
-    content,
-    "utf8"
-  );
+  try {
+    await fs.mkdir(path.resolve(process.cwd(), "./src"));
+    const content = `export * from '${argv.pkg}';`;
+    await fs.writeFile(
+      path.resolve(process.cwd(), "./src/index.ts"),
+      content,
+      "utf8"
+    );
+  } catch (e) {
+    process.exit(1);
+  }
 }
 
 createEntry();
--- scripts/install.js
+++ scripts/install.js
@@ -4,6 +4,7 @@ const childProcess = require("child_process");
 const util = require("util");
 const yargs = require("yargs");
 const fs = require("fs");
+const path = require("path");
 
 const argv = yargs.argv;
 const exec = util.promisify(childProcess.exec);
@@ -25,37 +26,41 @@ async function getTagName() {
 }
 
 async function execInstall() {
-  const tagName = await getTagName();
-  const version = semver.clean(tagName).split("-")[0];
-  const pkgName = argv.pkg;
+  try {
+    const tagName = await getTagName();
+    const version = semver.clean(tagName).split("-")[0];
+    const pkgName = argv.pkg;
 
-  await fs.promises.copyFile(
-    path.resolve(process.cwd(), "./package-lock.json"),
-    path.resolve(process.cwd(), "./package-lock-backup.json")
-  );
+    await fs.promises.copyFile(
+      path.resolve(process.cwd(), "./package-lock.json"),
+      path.resolve(process.cwd(), "./package-lock-backup.json")
+    );
 
-  const installResult = await exec(`npm install ${pkgName}@${version} -D`);
-  if (installResult.stderr) {
-    throw installResult.stderr;
-  } else {
-    console.log(installResult.stdout);
+    const installResult = await exec(`npm install ${pkgName}@${version} -D`);
+    if (installResult.stderr) {
+      throw installResult.stderr;
+    } else {
+      console.log(installResult.stdout);
+    }
+
+    const pkgContent = JSON.parse(
+      await fs.promises.readFile(
+        path.resolve(process.cwd(), "./package.json", "uf8")
+      )
+    );
+    pkgContent.version = semver.clean(tagName);
+    await fs.promises.writeFile(
+      path.resolve(process.cwd(), "./package.json"),
+      JSON.stringify(pkgContent)
+    );
+
+    await fs.promises.copyFile(
+      path.resolve(process.cwd(), "./package-lock-backup.json"),
+      path.resolve(process.cwd(), "./package-lock.json")
+    );
+  } catch (e) {
+    process.exit(1);
   }
-
-  const pkgContent = JSON.parse(
-    await fs.promises.readFile(
-      path.resolve(process.cwd(), "./package.json", "uf8")
-    )
-  );
-  pkgContent.version = semver.clean(tagName);
-  await fs.promises.writeFile(
-    path.resolve(process.cwd(), "./package.json"),
-    JSON.stringify(pkgContent)
-  );
-
-  await fs.promises.copyFile(
-    path.resolve(process.cwd(), "./package-lock-backup.json"),
-    path.resolve(process.cwd(), "./package-lock.json")
-  );
 }
 
 execInstall();
